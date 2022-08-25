29dd11e335afa285a7145574267d8559f418d754
Line-em <alineemily@hotmail.com>
2022-08-25 22:44:51 +0100

    base functions at context

--- src/Context/Cart/CartState.jsx
+++ src/Context/Cart/CartState.jsx
@@ -13,60 +13,57 @@ const CartContextProvider = ({ children }) => {
 	const [itemsInCart, setItemsInCart] = useState([]);
 	const allItems = goodReadsBooks.concat(featureBooks);
 
-	const addToCart = (currentBook) => {
-		setItemsInCart(allItems.map((book) => {}));
+	console.log("allItems " + allItems);
+
+	const manageCart = (currentBookId, actionType = "add") => {
+		let updatedItems = allItems.map((book) => {
+			currentBookId === book.id
+				? {
+						...book,
+						cart: (actionType === "add" && true) || (actionType === "remove" && false),
+						qntd: (actionType === "add" && 1) || (actionType === "remove" && 0)
+				  }
+				: book;
+		});
+		console.log("updatedItems manageCart" + updatedItems);
+		return setItemsInCart(updatedItems.filter((book) => book.cart));
 	};
-	const removeFromCart = "oi";
-	const increaseItem = "oi";
-	const decreaseItem = "oi";
+
+	const manageQntd = (currentBookId, actionType = "increase") => {
+		let updatedItems = itemsInCart.map((book) => {
+			currentBookId === book.id
+				? {
+						...book,
+						qntd: updateQntd(currentBookId, book.qntd, actionType)
+				  }
+				: book;
+		});
+
+		console.log("updatedItems manageQntd" + updatedItems);
+
+		const updateQntd = (id, qntd, action) => {
+			if (qntd === 0) {
+				return manageCart(id, "remove");
+			}
+			action === "increase" ? qntd++ : qntd--;
+		};
+
+		return setItemsInCart(updatedItems.filter((book) => book.cart));
+	};
+
+	const totalPrice = itemsInCart.reduce((acc, current) => acc + current.price * current.qntd, 0);
+	const totalItems = itemsInCart.reduce((acc, current) => acc + current.qntd, 0);
+
 	const checkout = "oi";
-
-	const totalPrice = "oi";
-
-	// const initalState = {
-	// 	cartItems: [],
-	// 	checkout: false
-	// };
-
-	// const [state, dispatch] = useReducer(CartReducer, initalState);
-
-	// const addToCart = (payload) => {
-	// 	dispatch({ type: "ADD_TO_CART", payload });
-	// };
-
-	// const increase = (payload) => {
-	// 	dispatch({ type: "INCREASE", payload });
-	// };
-
-	// const decrease = (payload) => {
-	// 	dispatch({ type: "DECREASE", payload });
-	// };
-
-	// const removeFromCart = (payload) => {
-	// 	dispatch({ type: "REMOVE_ITEM", payload });
-	// };
-
-	// const clearCart = () => {
-	// 	dispatch({ type: "CLEAR" });
-	// };
-
-	// const handleCheckout = () => {
-	// 	dispatch({ type: "CHECKOUT" });
-	// };
 
 	return (
 		<CartContext.Provider
 			value={{
-				addToCart
-				// cartItems: state.cartItems,
-				// showCart: state.showCart,
-				// addToCart,
-				// removeFromCart,
-				// increase,
-				// decrease,
-				// clearCart,
-				// handleCheckout,
-				// ...state
+				manageCart,
+				manageQntd,
+				checkout,
+				totalItems,
+				totalPrice
 			}}
 		>
 			{children}
